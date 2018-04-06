2e5e4a3c9911fd4f9194acfdc5883aef710aacb7
Tony Cannistra <tony.cannistra@gmail.com>
2018-04-06 12:48:00 -0700

    this is too time consuming

--- setup-staging.sh
+++ setup-staging.sh
@@ -1,4 +1,6 @@
 if [ "$TRAVIS_BRANCH" != "gh-pages" ]
 	then 
 		BASEURL=$TRAVIS_BRANCH envsubst < _config-staging.yml > _config.yml
+		echo "EDITED BASEURL TO $TRAVIS_BRANCH"
+		cat _config.yml
 fi
